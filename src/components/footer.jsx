728b2d2f1d7780acf92b6a0b1708cf5294071b08
Juneix Tse <81808039+juneix@users.noreply.github.com>
2024-04-30 14:25:24 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
           to={`https://github.com/${githubName}/`}
         />
         <CustomLink iconDom={<Home />} to={homeUrl} />
-        <CustomLink iconDom={<Mail />} to={`mailto:${emailUrl}`} />
+        <CustomLink iconDom={<Mail />} to={emailUrl} />
       </div>
       <div className="text">
         <p>
@@ -36,7 +36,7 @@ const Footer = () => {
         <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
+          <CustomLink to="https://5nav.eu.org/" text="谢週五の导航站" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
